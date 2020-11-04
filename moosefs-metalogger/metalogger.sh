a3323ca59f0e5a0d271db61f8d1c2d93634fad87
Piotr Robert Konopelko <piotr.konopelko@moosefs.pro>
2020-11-04 01:55:49 +0100

    Fixed Metalogger's startup script

--- moosefs-metalogger/metalogger.sh
+++ moosefs-metalogger/metalogger.sh
@@ -3,28 +3,8 @@
 # Set default MooseFS enviroment to PRODUCTION
 MFS_ENV="${MFS_ENV:-PROD}"
 
-#Set correct owner
+# Set a correct owner
 chown -R mfs:mfs /var/lib/mfs
 
-# We have to be sure that we have metadata files
-if [ -f /var/lib/mfs/metadata.mfs ];
-then
-    exec mfsmetalogger -f
-else
-    if [[ -f /var/lib/mfs/metadata.mfs.back.1 && -f /var/lib/mfs/changelog.0.mfs ]];
-    then
-        echo "Can't find metadata.mfs file"
-        echo "Let's try to restore it"
-        exec mfsmetalogger -a -f
-    else
-        if [  "$MFS_ENV" == "TEST" ];
-        then
-            echo "MFSM NEW" > /var/lib/mfs/metadata.mfs
-            exec mfsmetalogger -f
-        else
-            echo "No /var/lib/mfs/metadata.mfs file!"
-            echo "EXITING - THIS IS PRODUCTION ENVIRONMENT!"
-            exit 1
-        fi
-    fi
-fi
+# Run
+exec mfsmetalogger -f
